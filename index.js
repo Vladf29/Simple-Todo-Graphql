ce8908e4505f90c573f0f40025e8460ff8ef6d56
Vladyslav Ternovoi <vladislav.ternovoy@meliorgames.com>
2019-09-25 19:19:38 +0300

    [FIX] new random str

--- index.js
+++ index.js
@@ -36,3 +36,16 @@ function make_id(length) {
  }
 
  var strTmp = Make_id(12);
+ function generate_random_string(string_length){
+    let random_string = '';
+    let random_ascii;
+    let ascii_low = 65;
+    let ascii_high = 90
+    for(let i = 0; i < string_length; i++) {
+        random_ascii = Math.floor((Math.random() * (ascii_high - ascii_low)) + ascii_low);
+        random_string += String.fromCharCode(random_ascii)
+    }
+    return random_string
+}
+
+console.log(generate_random_string(5));
